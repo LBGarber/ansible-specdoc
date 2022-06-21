7924695111088a452520ebb225abe740eeb7c723
LBGarber <lgarber@linode.com>
2022-06-21 11:21:57 -0400

    Bump 0.0.9

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="ansible-specdoc",
-    version="0.0.8]9",
+    version="0.0.9",
     author="Lena Garber",
     author_email="lbgarber2@gmail.com",
     description=("A simple tool for generating Ansible collection documentation from module spec."),
